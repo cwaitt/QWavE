46e5af8d4de24b34e5b12a43a52419611bd04c0b
Craig Waitt <cwaitt@bastion.crc.nd.edu>
2021-03-21 01:53:59 -0400

    Initial commit after CMS Cookiecutter creation, version 1.5

---
+++ qwave/__init__.py
@@ -0,0 +1,14 @@
+"""
+qwave
+Quantum and Statistical mechanics solver
+"""
+
+# Add imports here
+from .functions import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
