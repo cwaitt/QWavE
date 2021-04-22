96e2e283f26428da8bdfb8bffd26d4deae51ac80
Audrey Miles <amiles2@nd.edu>
2021-04-21 21:29:22 -0400

    Added q_rot function. -AM

--- qwave/partition_func.py
+++ qwave/partition_func.py
@@ -19,8 +19,8 @@ plot (optional):
 from scipy import constants
 import numpy as np
 
-eV_to_J = constants.physical_constants['electron volt-joule relationship'][0]
-bohr_to_m = constants.physical_constants['Bohr radius'][0]
+# load internal modules
+from .plot import *
 
 def q_PESq(ei,temp,unit):
 
@@ -84,34 +84,38 @@ def q_HO(freq,temp,unit):
 
     return q_tot
 
-def q_HT(Vb,ax,mass,temp,unit):
-
-    if unit == 'eV':
+def q_rot(sigma, Ia, Ib, Ic, temp, unit):
+    
+    if unit == 'Hartree':
+        kb = constants.physical_constants['Boltzmann constant in eV/K'][0]/constants.physical_constants['Hartree energy in eV'][0]
+        h = -1*constants.physical_constants['Planck constant'][0]/constants.physical_constants['hartree-joule relationship'][0]
+    elif unit == 'eV':
         kb = constants.physical_constants['Boltzmann constant in eV/K'][0]
-        h = constants.physical_constants['Planck constant in eV s'][0]
+        h = -1*constants.physical_constants['Planck constant in eV s'][0]
+    elif unit == 'J':
+        kb = constants.physical_constants['Boltzmann constant'][0]  
+        h = -1*constants.physical_constants['Planck constant'][0]
+    elif unit == 'kJ/mol':
+        kb = constants.physical_constants['Boltzmann constant'][0]/1000*constants.N_A
+        h = -1*constants.physical_constants['Planck constant'][0]/1000*constants.N_A
     else:
-        raise ValueError('Unit must be eV')
-
-    print('Warning: make sure mass is in kg and ax is in bohr')
-
-    vx = ((Vb*eV_to_J)/(2*mass*(ax*bohr_to_m)**2))**0.5 # freqeuncy
-
-    rx = Vb/(h*vx)
-
-    Tx = (kb*temp)/(h*vx)
-
-    qclass = []
-    qHO = []
-    qzpe = []
-
-    for j in Tx:
-        qclass.append(np.sqrt((np.pi*rx/j))*np.exp(-1*rx/(2*j))*(np.i0(rx/(2*j))))
-        qHO.append(np.exp(-1/(2*j))/(1-np.exp(-1/j)))
-        qzpe.append(np.exp(1/((2+(16*rx))*j)))
-
-    q_tot = np.array(qclass)*np.array(qHO)*np.array(qzpe)
-    return q_tot
-
-
-
-
+        raise ValueError('Unit must be Hartree, eV, J, or kJ/mol')
+    
+    q_rot = []
+    
+    for i in temp:
+        print(i)
+        q_temp = 0
+        
+        coeff = ((8 * (np.pi**2) * kb * i) / (h**2))
+    
+        term1 = np.sqrt(np.pi) / sigma
+        term2 = np.sqrt(coeff * Ia)
+        term3 = np.sqrt(coeff * Ib)
+        term4 = np.sqrt(coeff * Ic)
+    
+        q_temp = term1 * term2 * term3 * term4
+        
+        q_rot = np.append(q_rot, q_temp)
+    
+    return q_rot
